6e64b72b28f752817f8f4e0c8016b69bcea75e8b
theNAFIZ <nrpustcse@gmail.com>
2022-01-02 07:37:03 +0600

    Part 1 completed - Buttons created and OnClick methods implemented.

--- app/src/main/java/com/example/petbio/MainActivity.java
+++ app/src/main/java/com/example/petbio/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.petbio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView catView;
+    private ImageView dogView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        catView = (ImageView) findViewById(R.id.catViewID);
+        dogView = (ImageView) findViewById(R.id.dogViewID);
+
+        catView.setOnClickListener(this);
+        dogView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.catViewID:
+                //TODO go to second activity
+                Toast.makeText(MainActivity.this, "Cat Touched", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.dogViewID:
+                //TODO go to second activity
+                Toast.makeText(MainActivity.this, "Dog Touched", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
