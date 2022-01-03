ceaeab27b6de9b7b3aefdac01b473ad38a06354d
theNAFIZ <nrpustcse@gmail.com>
2022-01-03 08:05:30 +0600

    MainActivity functionalities improved

--- app/src/main/java/com/example/petbio/MainActivity.java
+++ app/src/main/java/com/example/petbio/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.petbio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -30,11 +31,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.catViewID:
                 //TODO go to second activity
+                Intent catIntent = new Intent(MainActivity.this, BioActivity.class);
+                catIntent.putExtra("name","Cat Kyle");
+                catIntent.putExtra("bio","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ");
+                startActivity(catIntent);
                 Toast.makeText(MainActivity.this, "Cat Touched", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.dogViewID:
                 //TODO go to second activity
+                Intent dogIntent = new Intent(MainActivity.this, BioActivity.class);
+                dogIntent.putExtra("name","Dog Neil");
+                dogIntent.putExtra("bio","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ");
+                startActivity(dogIntent);
                 Toast.makeText(MainActivity.this, "Dog Touched", Toast.LENGTH_SHORT).show();
                 break;
 
